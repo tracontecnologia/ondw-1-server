daff26e0a40230998f5d150df8d1fbeb8bec84e2
Drinnn <pedro_lontro@hotmail.com>
2022-04-19 15:48:07 -0300

    chore: add db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeOrm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeOrmConfig),
+  ],
   controllers: [],
   providers: [],
 })
