f25713046bc8f39d95f5ad351cb3b74f9bd39e0d
Drinnn <pedro_lontro@hotmail.com>
2022-04-20 14:17:42 -0300

    refactor: remove update collection
    remove unused update collection service

--- src/domain/collection/collection.controller.ts
+++ src/domain/collection/collection.controller.ts
@@ -19,7 +19,6 @@ import { User } from '../user/user.entity';
 import { Collection } from './collection.entity';
 import { CollectionService } from './collection.service';
 import { CreateCollectionDto } from './dto/collection.create.dto';
-import { UpdateCollectionDto } from './dto/collection.update.dto';
 
 @Controller('collections')
 @UseGuards(AuthGuard())
@@ -42,13 +41,5 @@ export class CollectionController {
     @Body() createNFTDto: CreateNFTDto,
   ): Promise<NFT | null> {
     return this.service.createNFT(id, createNFTDto, photo.path);
-  }
-
-  @Put('/:id')
-  public async update(
-    @Param('id') id: string,
-    @Body() updateDto: UpdateCollectionDto,
-  ): Promise<Collection | null> {
-    return this.service.update(id, updateDto);
   }
 }
--- src/domain/collection/collection.service.ts
+++ src/domain/collection/collection.service.ts
@@ -7,7 +7,6 @@ import { NFTService } from '../nft/nft.service';
 import { User } from '../user/user.entity';
 import { Collection } from './collection.entity';
 import { CreateCollectionDto } from './dto/collection.create.dto';
-import { UpdateCollectionDto } from './dto/collection.update.dto';
 
 @Injectable()
 export class CollectionService {
@@ -51,32 +50,6 @@ export class CollectionService {
     );
 
     return nft;
-  }
-
-  public async update(
-    id: string,
-    updateDto: UpdateCollectionDto,
-  ): Promise<Collection | null> {
-    const { name } = updateDto;
-
-    const collection = await this.findById(id);
-    if (!collection)
-      throw new BadRequestException("There's no Collection with given ID");
-
-    const collectionWithSameName = await this.findByAuthorAndName(
-      collection.author,
-      name,
-    );
-    if (collectionWithSameName)
-      throw new BadRequestException(
-        "There's already another Collection with the same given name",
-      );
-
-    collection.name = name;
-
-    await collection.save();
-
-    return collection;
   }
 
   private async findById(id: string): Promise<Collection | null> {
