002fd4667595e458fa9e1c001ee75d04640081fa
Drinnn <pedro_lontro@hotmail.com>
2022-04-22 17:14:13 -0300

    chore: set CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.useStaticAssets('public', { prefix: '/public' });
 
